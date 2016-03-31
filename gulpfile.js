7527fd0b767bb4cb03e3e23065a36c1ef8932c01
Jared Lee <newleeland@gmail.com>
2016-03-31 18:43:10 +1300

    Gulp file setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
-var gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-    sass = require('gulp-sass');
+    var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+autoprefixer = require('gulp-autoprefixer');
 
 // Uglifies JS
 gulp.task('scripts', function(){
   gulp.src('JS/*.js')
+  .pipe(concat('build.js'))
+  .pipe(gulp.dest('JS'))
+  .pipe(rename('build.min.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('JS'));
+  .pipe(gulp.dest('JS/'));
 });
 
 // Compile SASS Task
 gulp.task('sass', function(){
   gulp.src('SCSS/*.scss')
-  .pipe(sass()
-  .on('error', sass.logError))
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(autoprefixer({browsers: ['last 2 versions', '> 5%', 'Firefox ESR']}))
   .pipe(gulp.dest('CSS/'));
 });
 
