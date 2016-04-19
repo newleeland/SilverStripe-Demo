a8779b52d67c1a6c5f2516c1be465a86ab57f223
Jared Lee <newleeland@gmail.com>
2016-04-19 14:34:11 +1200

    Added Login HTML

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,28 @@ autoprefixer = require('gulp-autoprefixer');
 
 // Uglifies JS
 gulp.task('scripts', function(){
-  gulp.src('JS/*.js')
+  gulp.src('js/*.js')
   .pipe(concat('build.js'))
-  .pipe(gulp.dest('JS/'))
+  .pipe(gulp.dest('js/'))
   .pipe(rename('build.min.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('JS/'));
+  .pipe(gulp.dest('js/'));
 });
 
 // Compile SASS Task
 gulp.task('sass', function(){
-  gulp.src('SCSS/**/*.scss')
+  gulp.src('scss/**/*.scss')
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(sourcemaps.write())
   .pipe(autoprefixer({browsers: ['last 2 versions', '> 5%', 'Firefox ESR']}))
-  .pipe(gulp.dest('CSS/'));
+  .pipe(gulp.dest('css/'));
 });
 
 // Watches JS and SASS
 gulp.task('watch', function(){
-    gulp.watch('JS/*.js', ['scripts']);
-    gulp.watch('SCSS/**/*.scss', ['sass']);
+    gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('scss/**/*.scss', ['sass']);
 });
 
 // Default Gulp action
